f92703368954258934207309586626d250130394
Cleo REBERT <crebert@e3r5p20.42.fr>
2021-01-24 16:40:27 +0100

    Better testing

--- main.c
+++ main.c
@@ -1,14 +1,41 @@
 #include "ft_printf.h"
 #include <stdio.h>
 
+#define test(format, ...) {\
+	standard = printf(format, __VA_ARGS__);\
+	ret = ft_printf(format, __VA_ARGS__);\
+	if (standard != ret)\
+		printf("Expected: %d got %d. Formatter: \"%s\"\n", standard, ret, format);\
+}
+
 int	main(void)
 {
-	ft_printf("%04i.\n", -12);
-	printf("%04i.\n", -12);
-	ft_printf("% .4i.\n", 12);
-	printf("% .4i.\n", 12);
-	ft_printf("%i.\n", -12);
-	printf("%i.\n", -12);
-	ft_printf("%0.i.\n", -12);
-	printf("%0.i.\n", -12);
+	int		standard;
+	int		ret;
+
+	test("%04i.\n", -12);
+	test("% .4i.\n", 12);
+	test("%i.\n", -12);
+	test("%0.i.\n", -12);
+	test("%s\n", "Hello?");
+	test("%s\n", NULL);
+	test("%.2s\n", NULL);
+	test("%s\n", "abcdefghijklmnop");
+	test("%c\n", 'b');
+	test("%c\n", '\0');
+	test("Je (%c) suis %s\n", 'F', "homosexuelle");
+	test("p0 % .3s\n", "cccc");
+	test("p1 %.4s\n", "cccc");
+	test("p2 %.10s\n", "cccc");
+	test("p3 %.4s\n", NULL);
+	test("p4 %.5s\n", "aaaaa");
+	test("st7 %*.*s\n", 1, 50, "hey");
+	test("st8 %*.*s\n", 0, 0, "coucou");
+	test("st9 %*.*s\n", 0, 50, "hey");
+	test("st10 %*.*s\n", 10, 0, "dsa");
+	test("st11 %*.*s\n", 10, 10, "hello");
+	test("st7 %0.*s\n", 50, "hey");
+	test("st8 %.*s\n", 0, "coucou");
+	test("st9 %*s\n", 0, "hey");
+	test("st10 %*s\n", 10, "dsa");
 }
