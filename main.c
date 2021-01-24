4e3e944d4fe5875b09cc5aba741699f3fe9f9f55
Cleo REBERT <crebert@e3r5p20.42.fr>
2021-01-24 17:25:53 +0100

    add tests

--- main.c
+++ main.c
@@ -17,12 +17,16 @@ int	main(void)
 	test("% .4i.\n", 12);
 	test("%i.\n", -12);
 	test("%0.i.\n", -12);
+
+	//c
+	test("%c\n", 'b');
+	test("%c\n", '\0');
+
+	// s
 	test("%s\n", "Hello?");
 	test("%s\n", NULL);
 	test("%.2s\n", NULL);
 	test("%s\n", "abcdefghijklmnop");
-	test("%c\n", 'b');
-	test("%c\n", '\0');
 	test("Je (%c) suis %s\n", 'F', "homosexuelle");
 	test("p0 % .3s\n", "cccc");
 	test("p1 %.4s\n", "cccc");
@@ -38,4 +42,35 @@ int	main(void)
 	test("st8 %.*s\n", 0, "coucou");
 	test("st9 %*s\n", 0, "hey");
 	test("st10 %*s\n", 10, "dsa");
+	test("st7 %*.*s\n", 1, 50, "hey");
+	test("st8 %*.*s\n", 0, 0, "coucou");
+	test("st9 %*.*s\n", 0, 50, "hey");
+	test("st10 %*.*s\n", 10, 0, "dsa");
+	test("st11 %*.*s\n", 10, 10, "hello");
+	test("st7 %0.*s\n", 50, "hey");
+	test("st8 %.*s\n", 0, "coucou");
+	test("st9 %*s\n", 0, "hey");
+	test("st10 %*s\n", 10, "dsa");
+	test("st118 %*s\n", 100, "hello");
+	test("st119 %*s\n", 1000, "hello");
+	test("st120 %*s\n", 10000, "hello");
+	test("st121 %*s\n", 100000, "hello");
+	test("neg1 %*s\n", -9, "coucou");
+	test("neg2 %*.*s\n", -9, 4, "coucou");
+	test("neg3 %*s\n", -9, NULL);
+	test("neg4 %*.*s\n", 0, 0, "coucou");
+	test("neg5 %*s\n", -100, "coucou");
+	test("neg6 %*s\n", -156, "coucou");
+	test("neg7 %*.*s\n", -1586, 15, "coucou");
+	test("neg8 %*.*s\n", -1586, 15, "coucou");
+	test("neg9 %*.*s\n", 15856, 155, "coucou");
+	test("neg10 %*.*s\n", -15586, 15, "coucou");
+	test("neg11 %*.*s\n", -15586, 15, "coucou");
+	
+	// x-X
+	test("ultimate4 %1.50x\n", 5000);
+	test("ultimate4Maj %1.0X\n", 100);
+	
+	// u
+	//test("ultimate5 %*.*u\n", 1, 50, 5000);
 }
