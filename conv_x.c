9d3484efc821158c2af34a6888138073dcc4445f
Cléo Rebert <cleo.rebert@gmail.com>
2021-03-25 22:10:17 +0100

    fix x

--- conv_x.c
+++ conv_x.c
@@ -28,11 +28,12 @@ void			conv_x(t_printf *pf, va_list args)
 		pf->format.flags &= ~PLUS;
 	num = va_arg(args, unsigned int);
 	len = BASE_16_LEN;
+	ft_bzero(str, BASE_16_LEN);
 	pf->format.infos = &len;
 	str_final = ft_itoa_base_pf(num, BASE_HEX_M, str, &(pf->format));
 	//printf("atoi: %d->%d->%s\n", ft_abs(num), num, str_final);
-	if (pf->format.flags & PLUS)
-		sign = '+';
+	if (!(sign = 0) && num && pf->format.flags & HASH)
+		sign = 'x';
 	else if (pf->format.flags & SPACE)
 		sign = ' ';
 	if (!num && pf->format.flags & PRECISION && !pf->format.precision)
@@ -52,11 +53,12 @@ void			conv_x_capital(t_printf *pf, va_list args)
 		pf->format.flags &= ~PLUS;
 	num = va_arg(args, unsigned int);
 	len = BASE_16_LEN;
+	ft_bzero(str, BASE_16_LEN);
 	pf->format.infos = &len;
 	str_final = ft_itoa_base_pf(num, BASE_HEX_C, str, &(pf->format));
 	//printf("atoi: %d->%d->%s\n", ft_abs(num), num, str_final);
-	if (pf->format.flags & PLUS)
-		sign = '+';
+	if (!(sign = 0) && num && pf->format.flags & HASH)
+		sign = 'X';
 	else if (pf->format.flags & SPACE)
 		sign = ' ';
 	if (!num && pf->format.flags & PRECISION && !pf->format.precision)
